64b3bd64c2f337d766ebc5eb80c05b99841f5433
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-05-22 22:13:53 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 232 ms, Memory: 49040 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dict = {}
+
+for _ in range(n):
+    a, b = input().split()
+    dict[a] = b
+
+for _ in range(m):
+    print(dict[input().rstrip()])
