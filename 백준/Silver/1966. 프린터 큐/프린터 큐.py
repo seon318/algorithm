acb062536e4e03e8cf6fff6d24aacf4d491b55c8
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-06-07 22:39:44 +0900

    [Silver III] Title: 프린터 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    
+    result = 1
+    while arr:
+        if arr[0] < max(arr):
+            arr.append(arr.pop(0))
+        else:
+            if m == 0:
+                break
+            arr.pop(0)
+            result += 1
+        
+        m = m - 1 if m > 0 else len(arr) - 1
+    print(result)
