d2b854bf61c74de0fda8b33084b3520d143c393e
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-07-13 22:27:21 +0900

    [Bronze V] Title: 이상한 기호, Time: 172 ms, Memory: 17652 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		System.out.println((a+b)*(a-b));
+		long a = sc.nextInt();
+		long b = sc.nextInt();
+		long ans = a*a - b*b;
+		System.out.println(ans);
 	}
 }
