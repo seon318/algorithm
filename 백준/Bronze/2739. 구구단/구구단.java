5c8a41d368c0a20856f714b5f4db547f12171da1
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-07-13 21:00:54 +0900

    [Bronze V] Title: 구구단, Time: 204 ms, Memory: 18596 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		int n = sc.nextInt();
 		for (int i = 1; i < 10; i++) {
-			System.out.println(num + " * " + i + " = " + num * i);
+			System.out.println(n + " * " + i + " = " + n * i);
 		}
 	}
 }
