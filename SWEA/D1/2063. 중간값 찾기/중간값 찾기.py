9cfe387d50cdd84f9e2b2fa8f0c1d9cdcd5e268c
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-07-30 12:08:39 +0900

    [D1] Title: 중간값 찾기, Time: 98 ms, Memory: 42,652 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,3 +1,23 @@
-n = int(input())
-score = sorted(list(map(int, input().split())))
-print(score[n // 2])
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+        
+		Arrays.sort(arr); //정렬
+		System.out.println(arr[arr.length/2]); //중간 인덱스 출력
+		
+	}
+}
