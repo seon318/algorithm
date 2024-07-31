34983f29f0f8d1d45264c73bd45060aa2efc800d
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-07-31 16:05:47 +0900

    [D2] Title: 숫자 배열 회전, Time: 110 ms, Memory: 43,492 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,18 +1,72 @@
-t = int(input())
-for x in range(1, t+1):
-    n = int(input())
-    matrix = [list(map(str, input().split())) for _ in range(n)]
-    result = [[], [], []]
-    
-    rotate = [[] for _ in range(n)]
-    for i in range(n-1, -1, -1):
-        for j in range(n):
-            rotate[j].append(matrix[i][j])
-    
-    print(f"#{x}")
-    for i in range(n):
-        ans = ''
-        ans += ''.join(rotate[i])
-        ans += ' '+''.join(reversed(matrix[n-1-i]))
-        ans += ' '+''.join(reversed(rotate[n-1-i]))
-        print(ans)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int t, n, tmp;
+	static int[][] arr, newArr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String[] str;
+
+		t = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= t; tc++) {
+			n = Integer.parseInt(br.readLine());
+			arr = new int[n][n];
+
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			str = new String[n];
+            
+			System.out.println("#" + tc);
+			// 90도씩 3번 돌려서 str 배열의 각 인덱스에 출력 형태로 넣기
+			for (int k=0; k<3; k++) {
+				arr = rotate();
+				for (int i = 0; i < n; i++) {
+					for (int j = 0; j < n; j++) {
+						if (k==0 && j==0) str[i] = "";
+						str[i] += arr[i][j];
+					}
+					str[i] += " ";
+				}
+			}
+			
+			for (int i=0; i<n; i++) {
+				System.out.println(str[i]);
+			}
+
+		}
+	}
+
+	static int[][] rotate() {
+		// 전치행렬
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i > j) {
+					tmp = arr[i][j];
+					arr[i][j] = arr[j][i];
+					arr[j][i] = tmp;
+				}
+			}
+		}
+		// 역으로 할당
+		newArr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				newArr[i][j] = arr[i][n - 1 - j];
+			}
+		}
+		// 90도 돌린 결과 반환
+		return newArr;
+	}
+
+}
