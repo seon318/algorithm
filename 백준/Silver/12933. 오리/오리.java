7a417dbad143ad9f65541aaf7a36252e98a39513
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-07 00:14:13 +0900

    [Silver II] Title: 오리, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -1,43 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String duck = br.readLine();
-		boolean isValid = true;
-		int ans = 0;
-		int[] freq = new int[5];
-		Map<Character, Integer> m = new HashMap<>();
-		m.put('q', 0);
-		m.put('u', 1);
-		m.put('a', 2);
-		m.put('c', 3);
-		m.put('k', 4);
-		out: for (char c : duck.toCharArray()) {
-			if (c == 'q') {
-				ans++;
-				if (freq[4] > 0) {
-					ans--;
-					for (int i = 0; i < 5; i++) {
-						freq[i]--;
-					}
-				}
-			}
-			freq[m.get(c)]++;
-			for (int i = 0; i < 4; i++) {
-				if (freq[i] < freq[i + 1]) {
-					isValid = false;
-					break out;
-				}
+		List<Integer> ducks = new ArrayList<>();
+		List<Character> alpha = new ArrayList<>(Arrays.asList('q', 'u', 'a', 'c', 'k'));
+		int x;
+		String input = br.readLine();
+		
+		for (int i = 0; i < input.length(); i++) {
+			x = alpha.indexOf(input.charAt(i));
+			if (ducks.contains(x-1)) ducks.set(ducks.indexOf(x-1), (x==4) ? -1 : x);
+			else if (x == 0) ducks.add(0);
+			else {
+				System.out.println(-1);
+				return;
 			}
 		}
-		for (int i = 0; i < 4; i++) {
-			if (freq[i] != freq[i + 1])
-				isValid = false;
+
+		for (int d : ducks) {
+			if (d != -1) {
+				System.out.println(-1);
+				return;
+			}
 		}
-		System.out.println(isValid ? ans : -1);
+		System.out.println(ducks.size());
 	}
+
 }
