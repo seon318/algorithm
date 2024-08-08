92390c7353cffd68c56745a71f3e17fb242f00d4
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-08 14:13:09 +0900

    [D2] Title: 숫자를 정렬하자, Time: 121 ms, Memory: 20,096 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -8,36 +10,36 @@ public class Solution {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-
 		int t = Integer.parseInt(br.readLine());
-		
-		for (int tc=1; tc<=t; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			int[] arr = new int[n];
+		int n, j, tmp;
+		int[] arr;
 
+		for (int tc = 1; tc <= t; tc++) {
+			n = Integer.parseInt(br.readLine());
+			arr = new int[n];
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i=0; i<n; i++) {
+			for (int i = 0; i < arr.length; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			for (int i=0; i<n; i++) {
-				int min = i;
-				for (int j=i; j<n; j++) {
-					if (arr[j] < arr[min]) {
-						min = j;
-					}
+			for (int i = 1; i < arr.length; i++) {
+				tmp = arr[i];
+				for (j = i - 1; j >= 0 && arr[j] > tmp; j--) {
+					arr[j+1] = arr[j];
 				}
-				int tmp = arr[i];
-				arr[i] = arr[min];
-				arr[min] = tmp;
+				arr[j+1] = tmp;
 			}
 			
-			System.out.print("#"+tc+" ");
-            for (int i=0; i<n; i++) {
-               System.out.print(arr[i]+" ");
-            }
-            System.out.println();
+			
+			bw.append("#" + tc + " ");
+			for (int i = 0; i < n; i++) {
+				bw.append(arr[i] + " ");
+			}
+			bw.append("\n");
 		}
+		bw.flush();
 	}
 }
