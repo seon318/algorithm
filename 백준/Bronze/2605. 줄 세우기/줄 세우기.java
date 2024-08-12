a6ef99680bbd7dccf824afe0b15925fe07e12173
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-13 08:58:01 +0900

    [Bronze II] Title: 줄 세우기, Time: 240 ms, Memory: 18476 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.java
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -1,25 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> list = new LinkedList<>();
-		int x;
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			x = Integer.parseInt(st.nextToken());
-			list.add(i - 1 - x, i);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		List<Integer> l = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			l.add(i - num, i + 1);
 		}
-		
-		for (int i = 0; i < n; i++) {
-			System.out.print(list.get(i) + " ");
+		for(int value : l) {
+		System.out.print(value + " ");
 		}
-	}
+}
 }
