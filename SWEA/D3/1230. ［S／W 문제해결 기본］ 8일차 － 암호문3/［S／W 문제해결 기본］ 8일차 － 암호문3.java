1112fe1e902384d703e74a8132e90a061a0d84fc
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-13 14:57:18 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 335 ms, Memory: 37,128 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		String command;
+		int n, c, x, y;
+		List<String> password = new LinkedList<>();
+
+		for (int tc = 1; tc <= 10; tc++) {
+			// 암호문 입력
+			password.clear();
+			n = Integer.parseInt(br.readLine());
+			password.addAll(Arrays.asList(br.readLine().split(" ")));
+
+			// 명령어 입력
+			c = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < c; i++) {
+				command = st.nextToken();
+				
+
+				// 명령어 수행
+				switch (command) {
+				case "I": {
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < y; j++) {
+						password.add(x + j, st.nextToken());
+					}
+					break;
+				}
+
+				case "D": {
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < y; j++) {
+						password.remove(x + 1);
+					}
+					break;
+				}
+
+				case "A": {
+					y = Integer.parseInt(st.nextToken());
+					for (int j = 0; j < y; j++) {
+						password.add(st.nextToken());
+					}
+					break;
+				}
+				}
+			}
+
+			// 결과 출력
+			sb = new StringBuilder();
+			sb.append("#" + tc + " ");
+			password.stream().limit(10).forEach(s -> sb.append(s).append(" "));
+			bw.append(sb).append("\n");
+		}
+
+		bw.flush();
+	}
+}
