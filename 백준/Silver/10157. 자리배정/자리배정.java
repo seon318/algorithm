a7a5cf9eb8c7b26f14ec120d59e4d0603741c2ca
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-14 13:39:59 +0900

    [Silver III] Title: 자리배정, Time: 208 ms, Memory: 24924 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -1,44 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int c = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(br.readLine());
-		int[][] arr = new int[c][r];
-		int[] dx = {0, 1, 0, -1};
-		int[] dy = {1, 0, -1, 0};
-		int x = 0;
-		int y = 0;
-		int nx, ny;
-		int d = 0;
-		int num = 1;
-		
-		if (c * r < k) {
-			System.out.println(0);
-			return;
-		}
-		
-		while (num < k) {
-			arr[x][y] = num++;
-			nx = x + dx[d];
-			ny = y + dy[d];
-			
-			if (0 > nx || nx >= c || 0 > ny | ny >= r || arr[nx][ny] != 0) {
-				d = ++d % 4;
-				nx = x + dx[d];
-				ny = y + dy[d];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		int R = sc.nextInt();
+		int cnt = 1;
+		int[][] arr = new int[C][R];
+		int top = 0;
+		int left = 0;
+		int right = C - 1;
+		int bottom = R - 1;
+		int K = sc.nextInt();
+		if (K <= R * C) {
+			out: while (cnt != C * R + 1) {
+				for (int r = top; r <= bottom; r++) {
+
+					arr[left][r] = cnt++;
+					if (cnt == K + 1) {
+						System.out.println((left + 1) + " " + (r + 1));
+						break out;
+					}
+				}
+				left++;
+
+				for (int c = left; c <= right; c++) {
+					arr[c][bottom] = cnt++;
+					if (cnt == K + 1) {
+						System.out.println((c + 1) + " " + (bottom + 1));
+						break out;
+					}
+				}
+				bottom--;
+
+				for (int r = bottom; r >= top; r--) {
+					arr[right][r] = cnt++;
+					if (cnt == K + 1) {
+						System.out.println((right + 1) + " " + (r + 1));
+						break out;
+					}
+				}
+				right--;
+				for (int c = right; c >= left; c--) {
+					arr[c][top] = cnt++;
+					if (cnt == K + 1) {
+						System.out.println((c + 1) + " " + (top + 1));
+						break out;
+					}
+				}
+				top++;
 			}
-			
-			x = nx;
-			y = ny;
+
+		} else {
+
+			System.out.println(0);
+
 		}
-		
-		System.out.println((x+1) + " " + (y+1));
+		sc.close();
 	}
 }
