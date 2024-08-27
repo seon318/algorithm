8558eec471398d0972e60173718380e938506385
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-27 17:13:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,402 ms, Memory: 83,960 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -8,8 +8,6 @@ public class Solution {
 	static int[][] matrix, arr;
 	static boolean[][] isBroken;
 	static int[] sel;
-	static int[] dx = { 1, -1, 0, 0 };
-	static int[] dy = { 0, 0, 1, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -114,20 +112,18 @@ public class Solution {
 
 	static void bomb(int x, int y, int k) {
 		arr[x][y] = 0;
-		for (int d = 0; d < 4; d++) {
-			for (int i = 0; i < k; i++) {
-				int nx = x + dx[d] * i;
-				int ny = y + dy[d] * i;
-				if (isValid(nx, ny)) {
-					bomb(nx, ny, arr[nx][ny]);
-				}
+		for (int i = x - k + 1; i < x + k; i++) {
+			if (i >= 0 && i < h) {
+				bomb(i, y, arr[i][y]);
 			}
 		}
-
-	}
-
-	static boolean isValid(int x, int y) {
-		return 0 <= x && x < h && 0 <= y && y < w;
+		
+		for (int i = y - k + 1; i < y + k; i++) {
+			if (i >= 0 && i < w) {
+				bomb(x, i, arr[x][i]);
+			}
+		}
+		
 	}
 
 }
