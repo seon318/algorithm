f1b49daa25d223e6df500aed038dd5f5700cf13b
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-08-28 00:13:21 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 68 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -6,79 +6,61 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-
-		int[] dx = { 0, 0, 0, -1, 1 };
-		int[] dy = { 0, 1, -1, 0, 0 };
-		int[] dice = new int[6];
-		int[] pair = {5, 4, 3, 2, 1, 0};
-
-		// top, north, east
-		int[] side = {0, 1, 2};
-
-		st = new StringTokenizer(br.readLine());
+		
+		//동서북남 
+		int[] dr = {0, 0, 0, -1, 1};
+		int[] dc = {0, 1, -1, 0, 0};
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-
-		int[][] arr = new int[n][m];
+		int tmp, nr, nc;
+		
+		int[][] matrix = new int[n][m];
+		
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				matrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
+		
+		int[] dice = {1, 3, 4, 2, 5, 6};
+		int[] value = new int[7];
+ 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < k; i++) {
-			int command = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
 
-			int nx = x + dx[command];
-			int ny = y + dy[command];
-
-			if (0 > nx || nx >= n || 0 > ny || ny >= m)
-				continue;
-
-			// 지도에서 해당 좌표 옮기고 이동 후 side 갱신 
-			x = nx;
-			y = ny;
+			nr = r + dr[d];
+			nc = c + dc[d];
 			
-			int tmp;
-			if (command == 1) {
-				tmp = side[0];
-				side[0] = pair[side[2]];
-				side[2] = tmp;
-			} else if (command == 2) {
-				tmp = side[2];
-				side[2] = pair[side[0]];
-				side[0] = tmp;
-			} else if (command == 3) {
-				tmp = side[0];
-				side[0] = pair[side[1]];
-				side[1] = tmp;
-			} else if (command == 4) {
-				tmp = side[1];
-				side[1] = pair[side[0]];
-				side[0] = tmp;
-			}
+			if (0 > nr || nr >= n || 0 > nc || nc >= m) continue;
+			
+			r = nr;
+			c = nc;
+			
+			tmp = dice[d];
+			dice[d] = dice[0];
+			dice[0] = 7 - tmp;
+			dice[d + (d % 2 == 0 ? -1 : 1)] = dice[5];
+			dice[5] = tmp;
 
-			sb.append(dice[side[0]]).append('\n');
-
-			// 아랫면 또는 지도 값 변화
-			int bottom = pair[side[0]];
-			if (arr[x][y] == 0) {
-				arr[x][y] = dice[bottom];
+									
+			if (matrix[r][c] == 0) {
+				matrix[r][c] = value[dice[0]];
 			} else {
-				dice[bottom] = arr[x][y];
-				arr[x][y] = 0;
+				value[dice[0]] = matrix[r][c];
+				matrix[r][c] = 0;
 			}
 			
+ 			sb.append(value[dice[5]]).append("\n");
 		}
-
-		System.out.println(sb);
-
+		
+		System.out.print(sb);
 	}
 }
