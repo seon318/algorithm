3894ec54d17c6f86ff2d59f947fdfc184725f03e
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-02 21:30:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 111 ms, Memory: 17,952 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,36 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-
+	static int p;
+	static int[] fare = new int[4];
+	static int[] month = new int[12];
+	static int[] dp = new int[12];
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
 		int t = Integer.parseInt(br.readLine());
-		int[] cost = new int[4];
-		int[] month = new int[13];
-		int[] dp = new int[13];
 		
 		for (int tc = 1; tc <= t; tc++) {
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < 4; i++) {
-				cost[i] = Integer.parseInt(st.nextToken());
+				fare[i] = Integer.parseInt(st.nextToken());
 			}
+			
 			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= 12; i++) {
-				month[i] = Math.min(Integer.parseInt(st.nextToken()) * cost[0], cost[1]);
+			for (int i = 0; i < 12; i++) {
+				p = Integer.parseInt(st.nextToken());
+				month[i] = Math.min(p * fare[0], fare[1]);
 			}
-
-			dp[1] = month[1];
-			dp[2] = month[2] + dp[1];
-			dp[3] = Math.min(month[3] + dp[2], cost[2]);
-			dp[4] = Math.min(month[4] + dp[3], cost[2] + month[1]);
-			for (int i = 4; i <= 12; i++) {
-				dp[i] = Math.min(month[i] + dp[i-1], cost[2] + dp[i - 3]);
+						
+			dp[0] = month[0];
+			dp[1] = month[0] + month[1];
+			dp[2] = Math.min(month[0] + month[1] + month[2], fare[2]);
+			for (int i = 3; i < 12; i++) {
+				dp[i] = Math.min(dp[i - 1] + month[i], dp[i - 3] + fare[2]);
 			}
 			
-			sb.append('#').append(tc).append(' ').append(Math.min(dp[12], cost[3])).append('\n');
+			sb.append("#").append(tc).append(" ").append(Math.min(dp[11], fare[3])).append("\n");
 		}
 		System.out.println(sb);
 	}
