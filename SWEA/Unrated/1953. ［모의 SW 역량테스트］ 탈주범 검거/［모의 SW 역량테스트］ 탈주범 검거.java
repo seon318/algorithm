5d82bdadbdea8d92d671f8ddf631576efa12e11a
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-02 22:04:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 152 ms, Memory: 24,384 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -6,91 +6,70 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n, m;
+	static int n, m, r, c, l, time, cnt; 
+	static int[][] map;
+	static int[] tunnel = {0, 15, 10, 5, 9, 3, 6, 12};
+	static int[] dr = {0, 1, 0, -1};
+	static int[] dc = {1, 0, -1, 0};
+	static boolean[][] visited;
+	static Queue<int[]> queue = new LinkedList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			l = Integer.parseInt(st.nextToken());
+			
+			map = new int[n][m];
+			visited = new boolean[n][m];
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < m; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());			}
+			}
+			
+			visited[r][c] = true;
+			queue.add(new int[] {r, c, 1});
+			while (!queue.isEmpty()) {
+				int[] item = queue.poll();
+				r = item[0];
+				c = item[1];
+				time = item[2];
+				if (time == l) break;
+				for (int d = 0; d < 4; d++) {
+					if ((1 << d & tunnel[map[r][c]]) > 0) {
+						int nr = r + dr[d];
+						int nc = c + dc[d];
+						if (isValid(nr, nc) && (1 << (d + 2) % 4 & tunnel[map[nr][nc]]) > 0) {
+							queue.add(new int[] {nr, nc, time + 1});
+							visited[nr][nc] = true;
+						}
+					}
+				}
+			}
+			queue.clear();
+			cnt = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (visited[i][j]) cnt++;
+				}
+			}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        int[] dx = { -1, 0, 1, 0 };
-        int[] dy = { 0, 1, 0, -1 };
-        int[] popItem;
-        int[][] map;
-        boolean[] now;
-        boolean[][] visited;
-        int r, c, l, ans;
-
-        for (int tc = 1; tc <= t; tc++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            r = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            l = Integer.parseInt(st.nextToken());
-            map = new int[n][m];
-            visited = new boolean[n][m];
-            ans = 0;  // 테스트 케이스마다 초기화 필요
-
-            for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < m; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            Queue<int[]> queue = new LinkedList<>();
-            queue.add(new int[] { r, c, 1 });
-            visited[r][c] = true; // 시작점 방문 처리
-
-            while (!queue.isEmpty()) {
-                popItem = queue.poll();
-                int curX = popItem[0];
-                int curY = popItem[1];
-                int time = popItem[2];
-
-                if (time == l) continue;
-
-                now = getPipe(map[curX][curY]);
-
-                for (int d = 0; d < 4; d++) {
-                    if (now[d]) {
-                        int nx = curX + dx[d];
-                        int ny = curY + dy[d];
-
-                        if (isValid(nx, ny) && !visited[nx][ny] && getPipe(map[nx][ny])[(d + 2) % 4]) {
-                            queue.add(new int[] { nx, ny, time + 1 });
-                            visited[nx][ny] = true;
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (visited[i][j])
-                        ans++;
-                }
-            }
-            sb.append("#").append(tc).append(" ").append(ans).append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    static boolean[] getPipe(int pipe) {
-        switch (pipe) {
-            case 1: return new boolean[] { true, true, true, true };
-            case 2: return new boolean[] { true, false, true, false };
-            case 3: return new boolean[] { false, true, false, true };
-            case 4: return new boolean[] { true, true, false, false };
-            case 5: return new boolean[] { false, true, true, false };
-            case 6: return new boolean[] { false, false, true, true };
-            case 7: return new boolean[] { true, false, false, true };
-            default: return new boolean[] { false, false, false, false };
-        }
-    }
-
-    static boolean isValid(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < m;
-    }
+			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static boolean isValid(int x, int y) {
+		return 0 <= x && x < n && 0 <= y && y < m && !visited[x][y];
+	}
 }
