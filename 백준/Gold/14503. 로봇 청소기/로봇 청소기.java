181eb07ccb4535d6c19f0284866e864930396024
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-02 15:34:39 +0900

    [Gold V] Title: 로봇 청소기, Time: 68 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] dr = {-1, 0, 1, 0};
+		int[] dc = {0, 1, 0, -1};
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		
+		int[][] room = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				room[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int ans = 0;
+		while (true) {
+			if (room[r][c] == 0) {
+				room[r][c] = -1;
+				ans++;
+			}
+			
+			int cnt = 0;
+			for (int k = 0; k < 4; k++) {
+				int nr = r + dr[k];
+				int nc = c + dc[k];
+				if (isValid(nr, nc) && room[nr][nc] == 0) cnt++;
+			}
+			
+			//청소되지 않은 칸 없는 경우
+			if (cnt == 0) {
+				int nr = r + dr[(d + 2) % 4];
+				int nc = c + dc[(d + 2) % 4];
+				if (isValid(nr, nc)) {
+					if (room[nr][nc] == 1) break;
+					r = nr;
+					c = nc;
+				} else {
+					break;
+				}
+			} else {
+				while (true) {
+					d = (d + 3) % 4;
+					int nr = r + dr[d];
+					int nc = c + dc[d];
+					if (isValid(nr, nc) && room[nr][nc] == 0) {
+						r = nr;
+						c = nc;
+						break;
+					}
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+	
+	static boolean isValid(int x, int y) {
+		return 0 <= x && x < n && 0 <= y && y < m;
+	}
+}
