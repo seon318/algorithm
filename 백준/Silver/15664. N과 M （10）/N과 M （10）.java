e119b2b6891c23f7aef79ff8a77223b23387ff47
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-03 23:08:11 +0900

    [Silver II] Title: N과 M (10), Time: 64 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] arr;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		numbers = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		comb(0, 0);
+		System.out.println(sb);
+	}
+	
+	static void comb(int cnt, int idx) {
+		if (cnt == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int past = 0;
+		
+		for (int i = idx; i < n; i++) {
+			if (past != arr[i]) {
+				numbers[cnt] = arr[i];
+				past = arr[i];
+				comb(cnt + 1, i + 1);
+			}
+			
+		}
+	}
+}
