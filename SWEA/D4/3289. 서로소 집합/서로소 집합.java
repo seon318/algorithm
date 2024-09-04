ef838bb212774b37f547e10444ca479d4830c858
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-04 14:22:24 +0900

    [D4] Title: 서로소 집합, Time: 424 ms, Memory: 101,232 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int n, m, x, a, b;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			arr = new int[n + 1];
+			for (int i = 1; i <= n; i++) {
+				arr[i] = i;
+			}
+
+			sb.append("#").append(tc).append(" ");
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				x = Integer.parseInt(st.nextToken());
+				a = findSet(Integer.parseInt(st.nextToken()));
+				b = findSet(Integer.parseInt(st.nextToken()));
+				
+				if (x == 0) {
+					if (a != b) arr[b] = a;
+				} else if (x == 1) {
+					sb.append(a == b ? 1 : 0);
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static int findSet(int x) {
+		if (arr[x] != x) {
+			arr[x] = findSet(arr[x]);
+		}
+		return arr[x];
+	}
+}
