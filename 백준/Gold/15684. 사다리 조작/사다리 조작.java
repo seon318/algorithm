11385dd2912cabf21619b0780525182b8db49f8d
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-05 23:12:01 +0900

    [Gold III] Title: 사다리 조작, Time: 500 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -2,29 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m, h, r, c, ans, idx, num;
-	static boolean[][] ladder, matrix;
-	static boolean isPossible;
-	static int[] sel = new int[3];
+	static boolean[][] ladder;
 	static List<int[]> info;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringBuilder sb = new StringBuilder();
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
-		
+
 		ladder = new boolean[h + 1][n + 1]; //n에서 0, n번 인덱스는 안 씀!
-		
+
 		//최초 사다리
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -32,81 +27,69 @@ public class Main {
 			c = Integer.parseInt(st.nextToken());
 			ladder[r][c] = true;
 		}
-
-		//추가 안 해도 되는지 확인
-		checkPossibility(0);
 		
-		if (!isPossible) {
+		//추가 안 해도 되는지 확인
+		if (!checkPossibility(0)) {
 			//사다리에 추가할 수 있는 가로선들의 정보는?
 			info = new ArrayList<>();
 			for (int i = 1; i < h + 1; i++) {
 				for (int j = 1; j < n; j++) {
-					if (isValid(i, j, ladder)) info.add(new int[] {i, j});
+					if (isValid(i, j)) info.add(new int[] {i, j});
 				}
 			}
+			
 			//사다리에 가로선 추가하는 모든 조합
-			isPossible = false;
 			ans = 4;
+			
 			for (int i = 1; i <= 3; i++) {
 				comb(0, 0, i);
-				if (ans < 4) break;
+				if (ans == i) break;
 			}
+			
 		}
 		
 		System.out.println(ans < 4  ? ans : -1);
 	}
 	
 	static void comb(int cnt, int idx, int max) {
-		if (ans < 4) return;
-		
+	
+		if (ans == max) return;	
+	
 		if (cnt == max) {
 			checkPossibility(max);
 			return;
 		}
-		
+	   
 		for (int i = idx; i < info.size(); i++) {
-			sel[cnt] = i;
+			if (!isValid(info.get(i)[0], info.get(i)[1])) continue;
+			ladder[info.get(i)[0]][info.get(i)[1]] = true;
 			comb(cnt + 1, i + 1, max);
+			ladder[info.get(i)[0]][info.get(i)[1]] = false;
 		}
+		
+			
 	}
-	
-	static void checkPossibility(int max) {
-		 matrix = new boolean[h + 1][n + 1];
-		 for (int i = 0; i < h + 1; i++) {
-			 for (int j = 0; j < n + 1; j++) {
-				 matrix[i][j] = ladder[i][j];
-			 }
-		 }
-		
-		for (int i = 0; i < max; i++) {
-			r = info.get(sel[i])[0];
-			c = info.get(sel[i])[1];
-			if (isValid(r, c, matrix)) matrix[r][c] = true;
-		}
-		
-		isPossible = true;
+	static boolean checkPossibility(int max) {
 		//모든 사다리가 자기자신으로 나오는지 확인
 		for (int i = 1; i < n; i++) {
 			idx = 1;
 			num = i;
 			while (idx < h + 1) {
-				if (matrix[idx][num]) {
+				if (ladder[idx][num]) {
 					num++;
-				} else if (matrix[idx][num - 1]) {
+				} else if (ladder[idx][num - 1]) {
 					num--;
 				}
 				idx++;
 			}
-			if (i != num) {
-				isPossible = false;
-				return;
-			}
+			if (i != num) return false;
 		}
 		
 		ans = Math.min(ans, max);
+		return true;
 	}
 	
-	static boolean isValid(int x, int y, boolean[][] arr) {
-		return !arr[x][y] && !arr[x][y - 1] && !arr[x][y + 1];
+	static boolean isValid(int x, int y) {
+		return !ladder[x][y] && !ladder[x][y - 1] && !ladder[x][y + 1];
 	}
 }
