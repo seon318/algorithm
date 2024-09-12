9cebe4bc81813a4f26311bacd222a14b19e1eb96
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-12 14:16:16 +0900

    [D3] Title: 햄버거 다이어트, Time: 184 ms, Memory: 20,504 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -4,49 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n, l, ans;
-	static int[][] ingredients;
-	static int[] selected;
-	
+	static int n, l;
+	static int[][] arr, dp;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		int t = Integer.parseInt(br.readLine());
-		
+		StringTokenizer st;
+
+		int t = Integer.parseInt(br.readLine().trim());
 		for (int tc = 1; tc <= t; tc++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine().trim());
 			n = Integer.parseInt(st.nextToken());
 			l = Integer.parseInt(st.nextToken());
-			
-			ingredients = new int[n][2];
-			for (int i = 0; i < n; i++) {
+			arr = new int[n + 1][2];
+			for (int i = 1; i <= n; i++) {
 				st = new StringTokenizer(br.readLine());
-				ingredients[i][0] = Integer.parseInt(st.nextToken());
-				ingredients[i][1] = Integer.parseInt(st.nextToken());
+				arr[i][0] = Integer.parseInt(st.nextToken());
+				arr[i][1] = Integer.parseInt(st.nextToken());
 			}
-			
-			ans = 0;
-			comb(0, 0, 0);
-						
-			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+
+			dp = new int[n + 1][l + 1];
+			for (int i = 1; i <= n; i++) {
+				for (int c = 1; c <= l; c++) {
+					if (arr[i][1] > c) dp[i][c] = dp[i-1][c];
+					else dp[i][c] = Math.max(dp[i-1][c], dp[i-1][c-arr[i][1]] + arr[i][0]);
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(dp[n][l]).append("\n");
 		}
-		
 		System.out.println(sb);
-	}
-	
-	static void comb(int score, int sum, int idx) {
-		for (int i = idx; i < n; i++) {
-			if (sum + ingredients[i][1] > l) {
-				continue;
-			}
-			score += ingredients[i][0];
-			sum += ingredients[i][1];
-			ans = Math.max(score, ans);
-			comb(score, sum, i + 1);
-			score -= ingredients[i][0];
-			sum -= ingredients[i][1];
-		}
 	}
 }
