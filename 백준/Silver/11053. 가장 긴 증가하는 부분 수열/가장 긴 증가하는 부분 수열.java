0227c018d74ceca308c494a50e2384e444e1972f
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-24 21:47:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] arr = new int[n];
-		for (int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int[] num = new int[n];
-		num[0] = 1;
+		int[] dp = new int[n];
+		dp[0] = 1;
 		int max;
-		
-		for (int i=1; i<n; i++) {
+		for (int i = 1; i < n; i++) {
 			max = 1;
-			for (int j=0; j<=i; j++) {
+			for (int j = 0; j <= i; j++) {
 				if (arr[i] > arr[j]) {
-					max = Math.max(max, num[j]+1);
+					max = Math.max(max, dp[j] + 1);
 				}
 			}
-			num[i] = max;
+			dp[i] = max;
 		}
 		max = 0;
-		for (int i=0; i<n; i++) {
-			max = Math.max(max, num[i]);
+		for (int i = 0; i < n; i++) {
+			max = Math.max(max, dp[i]);
 		}
 		System.out.println(max);
 	}
