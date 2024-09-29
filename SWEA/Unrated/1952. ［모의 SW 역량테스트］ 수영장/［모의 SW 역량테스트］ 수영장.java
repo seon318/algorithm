5e65640a936a12fc698c74aea3c62f2f6d3aa3d9
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-29 22:03:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 111 ms, Memory: 17,952 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,40 +4,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int p;
-	static int[] fare = new int[4];
-	static int[] month = new int[12];
-	static int[] dp = new int[12];
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
+		int[] fare = new int[4];
+		int[] dp = new int[13];
 		
 		for (int tc = 1; tc <= t; tc++) {
 			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < 4; i++) {
 				fare[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 12; i++) {
-				p = Integer.parseInt(st.nextToken());
-				month[i] = Math.min(p * fare[0], fare[1]);
-			}
-						
-			dp[0] = month[0];
-			dp[1] = month[0] + month[1];
-			dp[2] = Math.min(month[0] + month[1] + month[2], fare[2]);
-			for (int i = 3; i < 12; i++) {
-				dp[i] = Math.min(dp[i - 1] + month[i], dp[i - 3] + fare[2]);
+			for (int i = 1; i <= 12; i++) {
+				dp[i] = Math.min(Integer.parseInt(st.nextToken()) * fare[0], fare[1]);
 			}
 			
-			sb.append("#").append(tc).append(" ").append(Math.min(dp[11], fare[3])).append("\n");
+			dp[2] += dp[1];
+			for (int i = 3; i <= 12; i++) {
+				dp[i] = Math.min(dp[i] + dp[i-1], dp[i-3] + fare[2]);
+			}
+					
+			sb.append("#").append(tc).append(" ").append(dp[12] > fare[3] ? fare[3] : dp[12]).append("\n");
 		}
 		System.out.println(sb);
 	}
+	
 }
