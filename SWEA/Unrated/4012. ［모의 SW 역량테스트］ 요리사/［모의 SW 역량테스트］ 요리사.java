35ab6d5e21e9b2836d93072a7503fcfc26fd7450
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-29 21:57:30 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 246 ms, Memory: 22,472 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,66 +4,62 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n, ans;
-	static int[][] s;
-	static boolean[] selected;
-	
+	static int n, a, b, sum1, sum2, ans;
+	static boolean[] sel;
+	static int[][] arr;
+	static int[] sel1, sel2;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
 		int t = Integer.parseInt(br.readLine());
 		
 		for (int tc = 1; tc <= t; tc++) {
 			n = Integer.parseInt(br.readLine());
-			s = new int[n][n];
-			selected = new boolean[n];
-			
+			arr = new int[n][n];
+			sel = new boolean[n];
+			sel1 = new int[n/2];
+			sel2 = new int[n/2];
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
-					s[i][j] = Integer.parseInt(st.nextToken());
+				for (int j =0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
 			ans = Integer.MAX_VALUE;
 			comb(0, 0);
-			
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
-		
 		System.out.println(sb);
 	}
-
+	
 	static void comb(int cnt, int idx) {
-		if (cnt == n/2) {
-			ans = Math.min(getTasteDiff(), ans);
+		if (cnt == n / 2) {
+			a = 0;
+			b = 0;
+			for (int i = 0; i < n; i++) {
+				if (sel[i]) sel1[a++] = i;
+				else sel2[b++] = i;
+			}
+			sum1 = 0;
+			sum2 = 0;
+			for (int i = 0; i < n / 2 - 1; i++) {
+				for (int j = i + 1; j < n / 2; j++) {
+					sum1 += arr[sel1[i]][sel1[j]];
+					sum1 += arr[sel1[j]][sel1[i]];
+					sum2 += arr[sel2[i]][sel2[j]];
+					sum2 += arr[sel2[j]][sel2[i]];
+				}
+			}
+			ans = Math.min(ans, Math.abs(sum1 - sum2));
 			return;
 		}
 		
 		for (int i = idx; i < n; i++) {
-			 selected[i] = true;
-			 comb(cnt + 1, i + 1);
-			 selected[i] = false;
+			sel[i] = true;
+			comb(cnt + 1, i + 1);
+			sel[i] = false;
 		}
 	}
-	
-	static int getTasteDiff() {
-		int a = 0, b = 0;
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (selected[i] && selected[j]) {
-					a += s[i][j];
-					a += s[j][i];
-				} else if (!selected[i] && !selected[j]) {
-					b += s[i][j];
-					b += s[j][i];
-				}
-			}
-		}
-		return Math.abs(a - b);
-	}
-	
 }
