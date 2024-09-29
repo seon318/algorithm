cfb814ff823dc2b74f984e6b771ae2c4d23681c5
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-09-29 21:32:50 +0900

    [D4] Title: 치즈 도둑, Time: 278 ms, Memory: 37,212 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -4,7 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int n;
+	static int n, max, sum, ans;
+	static int[][] cheese;
 	static boolean[][] visited;
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
@@ -14,44 +15,42 @@ public class Solution {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
-		int cnt, ans;
-		int[][] cheese;
 
+		
 		for (int tc = 1; tc <= t; tc++) {
 			n = Integer.parseInt(br.readLine());
 			cheese = new int[n][n];
-			
+			max = 0;
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
 					cheese[i][j] = Integer.parseInt(st.nextToken());
+					max = Math.max(max, cheese[i][j]);
 				}
 			}
 			
 			ans = 1;
-			for (int day = 1; day <= 100; day++) {
+			for (int day = 1; day <= max; day++) {
+				sum = 0;
 				visited = new boolean[n][n];
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						if (cheese[i][j] <= day) visited[i][j] = true;
+						if (cheese[i][j] == day) cheese[i][j] = 0;
 					}
 				}
 				
-				cnt = 0;
 				for (int i = 0; i < n; i++) {
 					for (int j = 0; j < n; j++) {
-						if (!visited[i][j]) {
-							cnt++;
+						if (isValid(i, j)) {
+							sum++;
 							dfs(i, j);
 						}
 					}
 				}
-				if (cnt == 0) break;
-				ans = Math.max(cnt, ans);
+				ans = Math.max(sum, ans);
 			}
-			
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
-		} 
+		}
 		System.out.println(sb);
 	}
 	
@@ -59,14 +58,14 @@ public class Solution {
 		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
-			if (isValid(nx, ny) && !visited[nx][ny]) {
-				visited[nx][ny] = true;
-				dfs(nx, ny);
-			}
+			
+			if (!isValid(nx, ny)) continue;
+			visited[nx][ny] = true;
+			dfs(nx, ny);
 		}
 	}
 	
 	static boolean isValid(int x, int y) {
-		return 0 <= x && x < n && 0 <= y && y < n;
+		return x >= 0 && x < n && y >= 0 && y < n && cheese[x][y] != 0 && !visited[x][y];
 	}
 }
