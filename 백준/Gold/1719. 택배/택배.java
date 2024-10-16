da9f0dd004d0d866d2e83eff049c9f25de3b6b4c
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-10-16 19:02:27 +0900

    [Gold III] Title: 택배, Time: 256 ms, Memory: 19224 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,68 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node implements Comparable<Node> {
-		int v;
-		int c;
-		int start;
-		public Node(int v, int c, int start) {
-			super();
-			this.v = v;
-			this.c = c;
-			this.start = start;
-		}
-		@Override
-		public int compareTo(Node o) {
-			return this.c - o.c;
-		}
-		@Override
-		public String toString() {
-			return "Node [v=" + v + ", c=" + c + ", start=" + start + "]";
-		}
-		
-	}
-
-	public static void main(String[] args) throws IOException {
+	static int n, m, a, b, c;
+	static int[][] ans;
+	static int[][] time;
+	
+    public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
 		
-		ArrayList<Node>[] list = new ArrayList[n+1];
-		int[][] time = new int[n+1][n+1];
-		int[][] num = new int[n+1][n+1];
-		for (int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
-			Arrays.fill(time[i], n * 1000);
-		}
-		
-		for (int i = 0; i < m; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		ans = new int[n+1][n+1];
+		time = new int[n+1][n+1];
+		for (int i = 1; i <= n; i++) Arrays.fill(time[i], 1000*n);
+		for (int i = 1; i <= m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			list[a].add(new Node(b, c, b));
-			list[b].add(new Node(a, c, a));
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			if (time[a][b] > c) {
+				time[a][b] = c;
+				ans[a][b] = b;
+			}
+			if (time[b][a] > c) {
+				time[b][a] = c;
+				ans[b][a] = a;
+			}
 		}
 		
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		for (int i = 1; i <= n; i++) {
-			pq.add(new Node(i, 0, 0));
-			while (!pq.isEmpty()) {
-				Node poll = pq.poll();
-				for (Node node : list[poll.v]) {
-					int cost = poll.c + node.c;
-					if (time[i][node.v] > cost) {
-						time[i][node.v] = cost;
-						num[i][node.v] = poll.start == 0 ? node.v : poll.start;
-						pq.add(new Node(node.v, cost, num[i][node.v]));
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				if (k == i) continue;
+				for (int j = 1; j <= n; j++) {
+					if (k == j || i == j) continue;
+					if (time[i][j] > time[i][k] + time[k][j]) {
+						time[i][j] = time[i][k] + time[k][j];
+						ans[i][j] = ans[i][k];
 					}
 				}
 			}
@@ -71,11 +50,11 @@ public class Main {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (i == j) sb.append("- ");
-				else sb.append(num[i][j]).append(" ");
+				else sb.append(ans[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
-
+		
 		System.out.println(sb);
 	}
 }
