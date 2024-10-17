7392511e09bdab72417911de22783f2491799eff
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-10-17 11:44:26 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 168 ms, Memory: 36820 KB, Score: 1 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
@@ -1,57 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	static int n, m, k;
-	static char[][] matrix;
-	static int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};
-	static int[] dy = {1, -1, 1, -1, 0, 0, -1, 1};
-	static HashMap<String, Integer> map = new HashMap<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		
-		String input;
-		matrix = new char[n][m];
-		for (int i = 0; i < n; i++) {
-			input = br.readLine();
-			for (int j = 0; j < m; j++) {
-				matrix[i][j] = input.charAt(j);
-			}
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				perm(1, i, j, new StringBuilder().append(matrix[i][j]));
-			}
-		}
-		
-		for (int i = 0; i < k; i++) {
-			input = br.readLine();
-			if (!map.containsKey(input)) sb.append(0).append("\n");
-			else sb.append(map.get(input)).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
 
-	private static void perm(int cnt, int x, int y, StringBuilder sb) {
-		if (map.containsKey(sb.toString())) map.put(sb.toString(), map.get(sb.toString()) + 1);
-		else map.put(sb.toString(), 1);
-		if (cnt == 5) return;
-		
-		for (int d = 0; d < 8; d++) {
-			int nx = (x + dx[d] + n) % n;
-			int ny = (y + dy[d] + m) % m;
-			perm(cnt + 1, nx, ny, new StringBuilder(sb).append(matrix[nx][ny]));
+	// 8방향: 좌상, 상, 우상, 우, 우하, 하, 좌하, 좌
+	static int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 };
+	static int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
+	static int N, M, K;
+	static char[][] word;
+	static HashMap<String, Integer> count; // (단어, 등장 횟수) 저장  
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 격자 크기 N, M / 신이 좋아하는 문자열 개수 K
+		N = sc.nextInt();
+		M = sc.nextInt();
+		K = sc.nextInt();
+		sc.nextLine(); // 개행 제거
+
+		// 격자 
+		word = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			word[i] = sc.nextLine().toCharArray();
 		}
-	}
-}
+
+		// 찾을 문자열 
+		String[] words = new String[K];
+		for (int i = 0; i < K; i++) {
+			words[i] = sc.nextLine();
+		}
+
+		count = new HashMap<>();
+
+		// 각 셀에서 단어 탐색 시작
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				for (String w : words) { // 각 단어에 대해 탐색
+					searchWord(i, j, w); // 현재 위치에서 단어 찾기
+				}
+			}
+		}
+
+		// 결과 출력
+		for (String w : words) {
+			System.out.println(count.getOrDefault(w, 0));
+		}
+		
+	} // main 
+
+	// DFS | 시작 위치 x, y & 찾아야 하는 단어 
+	public static void searchWord(int x, int y, String w) {
+		int wordLen = w.length(); // 단어의 길이
+
+		// 8방향 탐색
+		for (int dir = 0; dir < 8; dir++) {
+			int nx = x; 
+			int ny = y; 
+			boolean found = true; // 단어 발견 여부 체크 
+
+			// 단어의 모든 문자에 대해 탐색
+			for (int k = 0; k < wordLen; k++) {
+				// 현재 위치의 문자와 목표 단어의 문자 비교
+				if (word[nx][ny] != w.charAt(k)) {
+					found = false; // 단어를 발견하지 못함
+					break;
+				}
+				// 다음 좌표 계산 (모듈로 연산으로 경계 처리)
+				nx = (nx + dx[dir] + N) % N;
+				ny = (ny + dy[dir] + M) % M;
+			}
+
+			// 단어를 찾았으면 카운트 증가
+			if (found) {
+				count.put(w, count.getOrDefault(w, 0) + 1);
+			}
+		}
+		
+	} // searchWord 
+} // class
