31f317d69090672f9854ad6c8226278598714aec
EunseonJeong <140936963+seon318@users.noreply.github.com>
2024-10-21 16:09:07 +0900

    [Gold II] Title: 보석 도둑, Time: 2452 ms, Memory: 327312 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,61 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Comparator;
 
 public class Main {
-	static int n, k;
-	static int[] c;
-	static PriorityQueue<Jewel> jewels = new PriorityQueue<>();
-	static PriorityQueue<Jewel> pq = new PriorityQueue<>((o1, o2) -> {
-		return o2.v - o1.v;
-	});
-	
-	static class Jewel implements Comparable<Jewel>{
-		int m, v;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 보석 개수
+        int K = sc.nextInt(); // 가방 개수
+        long sum = 0; // 총합
 
-		public Jewel(int m, int v) {
-			this.m = m;
-			this.v = v;
-		}
+        // 보석 정보를 저장할 배열 (무게, 가격)
+        int[][] jewels = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            jewels[i][0] = sc.nextInt(); // 보석 무게
+            jewels[i][1] = sc.nextInt(); // 보석 가격
+        }
 
-		@Override
-		public int compareTo(Jewel o) {
-			if (this.m != o.m) return this.m - o.m;
-			return o.v - this.v;
-		}
-		
-	}
-	
-    public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			jewels.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		
-		c = new int[k];
-		for (int i = 0; i < k; i++) {
-			c[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(c);
-		
-		long ans = 0;
-		int cnt = 0;
-		for (int i = 0; i < k; i++) {
-			while (!jewels.isEmpty() && jewels.peek().m <= c[i]) {
-				pq.add(jewels.poll());
-			}
-			if (!pq.isEmpty()) ans += pq.poll().v;
-		}
-		
-		System.out.println(ans);
+        // 가방 정보를 저장할 배열
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = sc.nextInt(); // 가방의 최대 무게
+        }
+
+        // 보석을 무게 기준 오름차순 정렬
+        Arrays.sort(jewels, Comparator.comparingInt(o -> o[0]));
+
+        // 가방을 무게 기준 오름차순 정렬
+        Arrays.sort(bags);
+
+        // 가방에 담을 수 있는 보석들을 저장할 우선순위 큐 (가격이 높은 순)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int jewelIdx = 0;
+        for (int i = 0; i < K; i++) {
+            int currentBag = bags[i]; // 현재 가방
+
+            // 현재 가방에 담을 수 있는 보석들을 우선순위 큐에 추가
+            while (jewelIdx < N && jewels[jewelIdx][0] <= currentBag) {
+                pq.add(jewels[jewelIdx][1]); // 보석의 가격 추가
+                jewelIdx++;
+            }
+
+            // 담을 수 있는 보석 중 가장 비싼 것을 선택
+            if (!pq.isEmpty()) {
+                sum += pq.poll();
+            }
+        }
+
+        // 최종 결과 출력
+        System.out.println(sum);
     }
-
 }
