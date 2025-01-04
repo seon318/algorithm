1765e45f0304d38d067df42aa262bee3e1b7e6cc
EunseonJeong <140936963+seon318@users.noreply.github.com>
2025-01-04 15:28:40 +0900

    [level 4] Title: 지형 편집, Time: 44.64 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/12984. 지형 편집/지형 편집.java
@@ -0,0 +1,44 @@
+public class Solution {
+    int[][] landArr;
+    int p;
+    int q;
+    
+    public long solution(int[][] land, int P, int Q) {
+        landArr = land;
+        p = P;
+        q = Q;
+                
+        long left = 1000000000;
+        long right = 0;
+        long mid = 0;
+        
+        //left를 최솟값, right를 최댓값으로 갱신
+        for (int[] arr: land) {
+            for (int x : arr) {
+                if (left > x) left = x;
+                if (right < x) right = x;
+            }
+        }
+        
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (calculateCost(mid) > calculateCost(mid + 1)) left = mid + 1;
+            else right = mid;
+        }
+        
+        long answer = calculateCost(left);
+        return answer;
+    }
+    
+    //비용 계산 함수
+    long calculateCost(long height) {
+        long sum = 0;
+        for (int[] arr : landArr) {
+            for (int x : arr) {
+                if (x < height) sum += (height - x) * p;
+                else if (x > height) sum += (x - height) * q;
+            }
+        }
+        return sum;
+    }
+}
