62c4887a12a132eb099b90a45c43aba798eab6d4
EunseonJeong <140936963+seon318@users.noreply.github.com>
2025-07-21 14:06:12 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N; cin >> N;
+  for (int i = 1; i <= N; i++) cout << string(N - i, ' ')<< string(2 * i - 1, '*') << '\n';
+  for (int i = 1; i < N; i++) cout << string(i, ' ')<< string(2 * (N - i) - 1, '*') << '\n';
+  return 0;
+}
